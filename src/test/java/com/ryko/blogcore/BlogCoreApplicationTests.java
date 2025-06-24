67617615159d7053021422005017d15be7ed2072
RayaneBICABA <rayanebicaba.dev@gmail.com>
2025-06-24 13:53:06 +0000

    Initial commit of BlogCore project

---
+++ src/test/java/com/ryko/blogcore/BlogCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryko.blogcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
